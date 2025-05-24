46bf5de1b13a1cc75bccb06cdec17b6255c92f20
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-05-24 19:01:21 +0700

    feat: create page.tsx

---
+++ app/bg-remover/page.tsx
@@ -0,0 +1,10 @@
+import BgRemoverPage from "./bgRemoverPage"
+
+export const metadata = {
+    title: "Background Remover",
+    description: "Background remover with OpenCV"
+}
+
+export default function Page() {
+    return <BgRemoverPage />
+}
